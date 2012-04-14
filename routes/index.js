f7312b15b907470ad0bdc6892c9ca4b0a3b8f561
Matt Bowcock <matt.bowcock@gmail.com>
2012-04-13 20:31:36 -0400

    fixed issue with rows returned from postgres.  Everytime page was refreshed the rows were added to page.  Issue related to missing query.on('end'...

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,17 @@
  */
 var pg = require('pg')
   , connectionString = 'postgres://postgres:513556@localhost/startups';
-var rows = [];
 
 client = new pg.Client(connectionString);
 client.connect();
 
 exports.index = function(req, res){
+	var rows=[];
 	query = client.query('select id, name, description, url, hiring, "hiringUrl" from startups');
 	query.on('row', function(row){
 		rows.push(row);
 	});
-	res.render('index', { dbRows: rows });
+	query.on('end', function(row){
+		res.render('index', { dbRows: rows });
+	});
 };
