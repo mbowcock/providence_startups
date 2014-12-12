378e24759fb3640c08752ff1da0b1fed48df6ab2
Matt Bowcock <matt.bowcock@gmail.com>
2014-12-11 21:09:25 -0500

    Lots of updates. updated express and jade to current stable. (A couple years worth of changes) Got some more testing to do but so far so good.

--- app.js
+++ app.js
@@ -1,29 +1,17 @@
 var express = require('express')
   , routes = require('./routes/routes.js');
 
-var app = module.exports = express.createServer();
-
+var app = express();
+var bodyParser = require('body-parser');
 // Configuration
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
+app.set('views', __dirname + '/views'); 
+app.set('view engine', 'jade');
+app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded());
+app.use(express.static(__dirname + '/public'));
 
 // Routes
-
 app.get('/', routes.index);
 app.get('/submit', routes.submit);
 app.post('/submit', routes.submitHandle);
@@ -31,5 +19,5 @@ app.get('/api', routes.show_documentation);
 app.get('/api/startups/:operation.:format', routes.api);
 
 app.listen(3001, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+  console.log("Providence startups starting up....");
 });
